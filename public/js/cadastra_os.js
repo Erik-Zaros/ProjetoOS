1952877feaafcd8240e3a3d2b8c6de1504edcf0a
Erik Delanda Zaros <erikzaros942@gmail.com>
2025-12-27 18:09:13 -0300

    erro carrega técnico

--- public/js/cadastra_os.js
+++ public/js/cadastra_os.js
@@ -5,6 +5,7 @@ $(document).ready(function () {
     if (osParam) {
       carregarDadosOS(osParam);
     } else {
+	  carregaTecnico();
       carregarProdutos();
       carregarServicosRealizados();
     }
