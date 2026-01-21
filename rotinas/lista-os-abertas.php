d93b3a582e93844640a4b7879f6a0908d1dd60c3
Erik Delanda Zaros <erikzaros942@gmail.com>
2026-01-20 21:41:26 -0300

    ajuste rotina

--- rotinas/lista-os-abertas.php
+++ rotinas/lista-os-abertas.php
@@ -8,22 +8,8 @@ $con = Db::getConnection();
 
 date_default_timezone_set('America/Sao_Paulo');
 
-$logDir  = '/var/www/log';
-if (!is_dir($logDir)) {
-    mkdir($logDir, 0775, true);
-}
-
-$logFile = $logDir . '/rotina_' . date('Y-m-d_H-i-s') . '.txt';
-
 $posto = $argv[1];
 $os = $argv[2] ?? null;
-
-function logMsg($msg) {
-    global $logFile;
-    file_put_contents($logFile, $msg, FILE_APPEND);
-}
-
-logMsg("Parâmetros recebidos - Posto: $posto | OS: $os");
 
 function buscaOs($posto, $os = null) {
 
@@ -34,7 +20,7 @@ function buscaOs($posto, $os = null) {
 		$cond = " AND tbl_os.os = $os ";
 	}
 
-	$sql = "SELECT os, data_abertura
+	$sql = "SELECT os, to_char(data_abertura, 'DD/MM/YYYY') as data_abertura
 			FROM tbl_os
 			WHERE posto = {$posto}
 			{$cond}
@@ -53,18 +39,15 @@ try {
 	$oss = buscaOs($posto, $os);
 
 	if ($oss == null) {
-		logMsg("Nenhuma OS encontrada");
         echo "Nenhuma OS encontrada";exit;
 	}
 
 	foreach ($oss as $dado) {
-		logMsg("OS: {$dado['os']} | Data Abertura: {$dado['data_abertura']}");
         echo "OS: {$dado['os']}\n";
         echo "Data Abertura: {$dado['data_abertura']}\n";
 	}
 
 } catch (Throwable $e) {
-    logMsg('Erro: ' . $e->getMessage());
     echo "Erro ao Executar Rotina";
 }
 
